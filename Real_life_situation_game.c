6e033e9137bd4c220a7623f747af1aae8a522eca
Pradeep <knp2jd@gmail.com>
2023-03-19 16:39:34 +0530

    Improved river

--- Real_life_situation_game.c
+++ Real_life_situation_game.c
@@ -1,35 +1,10 @@
 #include<graphics.h>
 #include<conio.h>
 
-void river()
-{  int xmax,ymax;
-
-   xmax = getmaxx();
-   ymax = getmaxy();
-   ellipse(xmax-170,100,260,360,150,70);
-   ellipse(xmax-160,120,260,360,140,70);
-
-   ellipse(xmax-198,238,90,175,150,70);
-   ellipse(xmax-185,240,90,175,125,50);
-
-   ellipse(xmax-495,223,270,350,150,50);
-   ellipse(xmax-440,237,300,360,130,50);
-   line(264,281,160,305);
-   ellipse(110,339,70,170,91,70);
-   ellipse(130,374,75,170,110,70);
-
-}
-
-
-void map()
-{
-  int xmax = getmaxx();
-  int ymax = getmaxy();
-
-  rectangle(20,20,xmax-20,ymax-60);
-  river();
-
-}
+void loading_animation();
+void river_of_map1();
+void map1();
+void story_1();
 
 void main()
 {
@@ -42,10 +17,55 @@ void main()
    ymax = getmaxy();
    rectangle(0,0,xmax,ymax);
 
+   loading_animation();
 
-   map();
+   story_1();
+
 
 
    getch();
+
    closegraph();
 }
+
+void river_of_map1()
+{  int xmax,ymax;
+
+   xmax = getmaxx();
+   ymax = getmaxy();
+   ellipse(xmax-170,100,260,360,150,70);
+   ellipse(xmax-160,120,260,340,150,70);
+
+   ellipse(xmax-198,238,90,175,150,70);
+   ellipse(xmax-185,240,90,175,125,50);
+
+   ellipse(xmax-495,223,270,350,150,50);
+   ellipse(xmax-440,237,300,360,130,50);
+   line(xmax-460,295,xmax-375,280);
+
+   ellipse(xmax-508,357,85,145,134,85);
+   ellipse(xmax-469,376,87,152,169,81);
+
+}
+
+
+void map1()
+{
+  int xmax = getmaxx();
+  int ymax = getmaxy();
+
+  rectangle(20,20,xmax-20,ymax-60);
+  river_of_map1();
+
+}
+void story_1()
+{
+  map1();
+
+}
+void loading_animation()
+{
+
+
+}
+
